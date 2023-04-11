e79212f240d3affb79f48ffb6633385311654d8d
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-04-11 13:22:25 +0000

    Update index.ts

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const todoList = document.querySelector<HTMLUListElement>('#list');
+const todoForm = document.querySelector<HTMLFormElement>('#newTaskForm');
+const todoInput = document.querySelector<HTMLInputElement>('#newTaskTitle');
