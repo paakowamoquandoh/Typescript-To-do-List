bbc04f41b73b964b86486bd492fb2bfc0c808b15
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-04-11 15:11:27 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ const todoList = document.querySelector<HTMLUListElement>('#list');
 const todoForm = document.querySelector<HTMLFormElement>('#newTaskForm');
 const todoInput = document.querySelector<HTMLInputElement>('#newTaskTitle');
 
+const todos: TaskType[] = loadTodos()
+todos.forEach(addTodoItem)
 
 todoForm?.addEventListener("submit", e => {
   e.preventDefault()
@@ -23,9 +25,33 @@ todoForm?.addEventListener("submit", e => {
     completed: false,
     createdAt: new Date()
   }
+  todos.push(newTodo)
+
   addTodoItem(newTodo)
+  todoInput.value = ""
 })
 
 function addTodoItem(todo: TaskType){
-  
+  const todoItem = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", ()=>{
+    todo.completed = checkbox.checked
+    saveTodos()
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = todo.completed
+  label.append(checkbox, todo.title)
+  todoItem.append(label)
+  todoList?.append(todoItem)
+}
+
+function saveTodos() {
+  localStorage.setItem("TODOS", JSON.stringify(todos))
+}
+
+function loadTodos(): TaskType[] {
+  const todoJSON = localStorage.getItem("TODOS")  
+  if(todoJSON == null) return []
+  return JSON.parse(todoJSON )
 }
