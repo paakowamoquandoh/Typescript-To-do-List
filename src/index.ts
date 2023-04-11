5e4d5910f307d9ab47a06b0326f96bb169c8179c
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-04-11 14:34:24 +0000

    index update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
+import {v4 as uuidV4  } from "uuid";
+
+type TaskType = {
+  id: string,
+  title: string,
+  completed: boolean,
+  createdAt: Date
+}
+
 const todoList = document.querySelector<HTMLUListElement>('#list');
 const todoForm = document.querySelector<HTMLFormElement>('#newTaskForm');
 const todoInput = document.querySelector<HTMLInputElement>('#newTaskTitle');
+
+
+todoForm?.addEventListener("submit", e => {
+  e.preventDefault()
+  
+  if(todoInput?.value == "" || todoInput?.value == null) return
+
+  const newTodo: TaskType = {
+    id: uuidV4(),
+    title: todoInput.value,
+    completed: false,
+    createdAt: new Date()
+  }
+  addTodoItem(newTodo)
+})
+
+function addTodoItem(todo: TaskType){
+  
+}
